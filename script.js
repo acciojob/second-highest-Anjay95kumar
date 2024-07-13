e2c214977596fb89b1c62fbf62084541b4e332a4
Anjay95kumar <130925026+Anjay95kumar@users.noreply.github.com>
2024-07-13 11:46:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length <= 1) {
+        return -Infinity;
+    }
+    
+    let firstMax = -Infinity;
+    let secondMax = -Infinity;
+    
+    for (let num of arr) {
+        if (num > firstMax) {
+            secondMax = firstMax;
+            firstMax = num;
+        } else if (num > secondMax && num < firstMax) {
+            secondMax = num;
+        }
+    }
+    
+    return secondMax !== -Infinity ? secondMax : -Infinity;
+}
